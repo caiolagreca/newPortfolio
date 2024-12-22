c7e24cff52529fae523a4cacdd550275af2eafdc
Caio Lagreca <caiomiranda701@hotmail.com>
2024-12-23 06:56:47 +1000

    applying lazy loading

--- frontend/src/Hooks/useArticles.tsx
+++ frontend/src/Hooks/useArticles.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { getArticleService } from "../Services/ArticleService";
 import { Article } from "../Types/Article";
 
@@ -6,21 +6,47 @@ const useArticles = () => {
 	const [articles, setArticles] = useState<Article[]>([]);
 	const [serverError, setServerError] = useState<string | null>(null);
 	const [loading, setLoading] = useState<boolean>(true);
+	const [hasFetched, setHasFetched] = useState(false);
+
+	const containerRef = useRef<HTMLElement>(null);
 
 	useEffect(() => {
-		const getArticles = async () => {
-			const result = await getArticleService();
-			if (typeof result === "string") {
-				setServerError(result);
-			} else if (Array.isArray(result.data)) {
-				setArticles(result.data);
-			}
-			setLoading(false);
-		};
-		getArticles();
-	}, []);
+		const observer = new IntersectionObserver((entries) => {
+			entries.forEach((entry) => {
+				if (entry.isIntersecting && !hasFetched) {
+					setHasFetched(true);
+					setLoading(true);
 
-	return { articles, serverError, loading };
+					getArticleService()
+						.then((result) => {
+							if (typeof result === "string") {
+								setServerError(result);
+								setLoading(false);
+							} else if (Array.isArray(result.data)) {
+								setArticles(result.data);
+								setLoading(false);
+							}
+						})
+						.catch((err) => {
+							setServerError(`Error fetching Skills: ${err}`);
+							setLoading(false);
+						});
+				}
+			});
+		});
+
+		if (containerRef.current) {
+			observer.observe(containerRef.current);
+		}
+
+		return () => {
+			if (containerRef.current) {
+				observer.unobserve(containerRef.current);
+			}
+		};
+	}, [hasFetched]);
+
+	return { articles, serverError, loading, containerRef };
 };
 
 export default useArticles;
